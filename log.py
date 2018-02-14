6f48529a5b6d14deda077fb7f46058c549b03e1f
Curtis Adkins <curtis@wayne-manor.gotham>
2018-02-14 08:27:04 -0500

    reverting log format

--- log.py
+++ log.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python
 
 import logging
-from logging.handlers import TimedRotatingFileHandler
 import os
+from logging.handlers import TimedRotatingFileHandler
+
+diamond_back_home = os.path.expanduser(os.path.join('~/.config', 'diamondback'))
+diamond_back_log = diamond_back_home + "/" + "dback.log"
 
 def log_setup():
 
-    diamond_back_home = os.path.expanduser(os.path.join('~/.config', 'diamondback'))
     logger = logging.getLogger("DiamondBack")
-    hdlr = TimedRotatingFileHandler(diamond_back_home + "/" + "dback.log",when = "D",interval = 30,backupCount = 30)
+    logger.setLevel(logging.DEBUG)
+    
+    hdlr = TimedRotatingFileHandler(diamond_back_log,when="D",interval=30,backupCount=30)
+
     formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
     hdlr.setFormatter(formatter)
+    
     logger.addHandler(hdlr)
-    logger.setLevel(logging.DEBUG)
 
     return logger
+
