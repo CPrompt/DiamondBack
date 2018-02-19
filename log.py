702df3c5eef4f82e1392dc921f86a6ecfaaf8ebe
Curtis Adkins <curtis@wayne-manor.gotham>
2018-02-19 16:27:06 -0500

    Addking comments

--- log.py
+++ log.py
@@ -9,28 +9,13 @@ from logging.handlers import WatchedFileHandler
 diamond_back_home = os.path.expanduser(os.path.join('~/.config', 'diamondback'))
 diamond_back_log = diamond_back_home + "/" + "dback.log"
 
-'''
-def log_setup():
-    logger = logging.getLogger("DiamondBack")
-    logger.setLevel(logging.DEBUG)
-
-    hdlr = TimedRotatingFileHandler(diamond_back_log,when="D",interval=30,backupCount=30)
-
-    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
-    hdlr.setFormatter(formatter)
-
-    logger.addHandler(hdlr)
-
-    return logger
-'''
-
 def log_setup():
 
     logger = logging.getLogger("DiamondBack")
     logger.setLevel(logging.DEBUG)
 
     hdlr = WatchedFileHandler(diamond_back_log)
-    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s") 
     hdlr.setFormatter(formatter)
 
     logger.addHandler(hdlr)
