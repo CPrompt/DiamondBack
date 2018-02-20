b3a1f552aaabf211f57341ea7bcdcc75b1f4d37b
Curtis Adkins <curtis@wayne-manor.gotham>
2018-02-20 08:29:37 -0500

    Working through making the logging better

--- purge_files.py
+++ purge_files.py
@@ -20,6 +20,9 @@ old_logfiles = glob.glob(os.path.join(log_directory, '*.*'))
 current_date = str.split(time.strftime("%Y %m"))
 current_weekday = str.split(time.strftime("%d"))
 
+# list to hold the files that will be deleted
+files_found = []
+
 # This section will take the current date and determine
 # what day of the week was the first day (0 = Monday, 1 = Tuesday, etc...)
 # Then it will tell how many days in the given month
@@ -45,28 +48,31 @@ def remove_old_files():
         # This just splits the current date so we can parse it out
         dic_date = time_split.split("-")
 
-        # we only want this to process on the last day of the month
-        if(current_day_in_month == days_in_month):
-            # if the month of the file is the same as the current month
-            if(int(dic_date[1]) == int(current_date[1])):
-                # if the day of the log file is less than the last day of the month
-                #if(int(dic_date[2]) < int(determine_last_day(current_date))):
-                if(int(dic_date[2]) < int(days_in_month)):
-                    # here we are going to try to delete records.
-                    #print(old_logs)
-                    try:
-                        logger.info("Removing old files....")
-                        os.remove(old_logs)
-                    except:
-                        logger.error("Could not remove files")
-                else:
-                    logger.info("No files to remove")
+    if(current_day_in_month == days_in_month):
+        if(int(dic_date[1] == int(current_date[1]))):
+            if(int(dic_date[2]) < int(days_in_month)):
+                try:
+                    # os.remove(old_logs)
+                    # any files found, add them to the list
+                    files_found.append(old_logs)
+                except:
+                    logger.warning("Could not remove files!")
+            else:
+                files_found.count == 0
         else:
-            logger.info("No files to remove")
+            files_found.count == 0
+    else:
+        files_found.count == 0
 
 
+    if files_found.count == 0:
+        logger.info("No files found to be removed")
+    else:
+        logger.info("Files remove : " + str(old_logs))
 
 
+if __name__ == "__main__":
+    remove_files()
 
 
 
