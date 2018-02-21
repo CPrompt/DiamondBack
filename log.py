48972f303dd073a79b78cc896dc7f9c2c03e4a2a
Curtis Adkins <curtis@wayne-manor.gotham>
2018-02-21 08:18:44 -0500

    Testing why logging is writing info 3 times

--- log.py
+++ log.py
@@ -2,8 +2,6 @@
 
 import os
 import logging
-#from logging.handlers import TimedRotatingFileHandler
-#from logging.handlers import RotatingFileHandler
 from logging.handlers import WatchedFileHandler
 
 diamond_back_home = os.path.expanduser(os.path.join('~/.config', 'diamondback'))
@@ -11,11 +9,11 @@ diamond_back_log = diamond_back_home + "/" + "dback.log"
 
 def log_setup():
 
-    logger = logging.getLogger("DiamondBack")
+    logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
 
     hdlr = WatchedFileHandler(diamond_back_log)
-    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s") 
+    formatter = logging.Formatter("[%(filename)s:%(lineno)s - %(funcName)20s()] %(asctime)s %(levelname)s %(message)s") 
     hdlr.setFormatter(formatter)
 
     logger.addHandler(hdlr)
--- purge_files.py
+++ purge_files.py
@@ -7,18 +7,24 @@ import datetime
 from datetime import date
 import calendar
 from read_json import output_config
-from config import backupName, directory_of_backup, redundant_backup_directory, files_for_backup, ignored_files
-from log import *
+from config import directory_of_backup
+from log import log_setup
 
+'''
+import logging
+from logging.handlers import WatchedFileHandler
 diamond_back_home = os.path.expanduser(os.path.join('~/.config', 'diamondback'))
-logger = log_setup()
+diamond_back_log = diamond_back_home + "/" + "dback.log"
+'''
+
 log_directory = directory_of_backup
 old_logfiles = glob.glob(os.path.join(log_directory, '*.*'))
 
-
 # current year and month
 current_date = str.split(time.strftime("%Y %m"))
+current_year = str.split(time.strftime("%Y"))
 current_weekday = str.split(time.strftime("%d"))
+
 
 # This section will take the current date and determine
 # what day of the week was the first day (0 = Monday, 1 = Tuesday, etc...)
@@ -28,13 +34,32 @@ def determine_last_day(this_date):
     current_calendar = calendar.monthrange(int(this_date[0]),int(this_date[1]))
     # we just need to return the number of days in this month
     return current_calendar[1]
+'''
+def log_setup():
 
+    #logger = logging.getLogger("DiamondBack")
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    hdlr = WatchedFileHandler(diamond_back_log)
+    formatter = logging.Formatter("[%(filename)s:%(lineno)s - %(funcName)20s()] %(asctime)s %(levelname)s %(message)s") 
+    hdlr.setFormatter(formatter)
+
+    logger.addHandler(hdlr)
+
+    return logger
+'''
 
 def remove_old_files():
+
+    logger = log_setup()
+    message = ""
 
     current_day_in_month = int(current_weekday[0])
     days_in_month = determine_last_day(current_date)
 
+    # list to hold the files found that are ready to be purged
+    files_found = []
 
     for old_logs in old_logfiles:
         stat_info = os.stat(old_logs).st_mtime
@@ -46,25 +71,47 @@ def remove_old_files():
         dic_date = time_split.split("-")
 
         # we only want this to process on the last day of the month
-        if(current_day_in_month == days_in_month):
+        if(dic_date[0] == current_year):
+            if(current_day_in_month == days_in_month):
+                files_found.append(old_logs)
+            else:
+                message = "No Files"
+                message = message + " : " + old_logs
+        else:
+            message = "All loops ended"
+            '''
             # if the month of the file is the same as the current month
             if(int(dic_date[1]) == int(current_date[1])):
                 # if the day of the log file is less than the last day of the month
-                #if(int(dic_date[2]) < int(determine_last_day(current_date))):
                 if(int(dic_date[2]) < int(days_in_month)):
                     # here we are going to try to delete records.
-                    #print(old_logs)
                     try:
-                        logger.info("Removing old files....")
+                        #logger.info("Removing old files....")
+                        files_found.append(old_logs)
                         # going to comment this out for testing
                         # os.remove(old_logs)
                     except:
                         logger.error("Could not remove files")
                 else:
-                    logger.info("No files to remove")
+                    message = "First if"
+            else:
+                message = "Second if"
         else:
-            logger.info("No files to remove")
+            message = "Third if " + old_logs
+            '''
 
+    if (len(files_found) == 0):
+        print(message)
+        logger.info(message)
+    else:
+        print(message)
+        logger.warning("WARNING!")
+        logger.info(files_found)
+    
+    
+
+if __name__ == "__main__":
+    remove_old_files()
 
 
 
